fef258aef736ca2f9b071c92a2182de29c9fcae8
adekmzrk <adekmzrk@gmail.com>
2026-09-14 08:25:55 +0700

    update tooltip theme

--- src/shared/components/ui/tooltip.tsx
+++ src/shared/components/ui/tooltip.tsx
@@ -41,7 +41,7 @@ function TooltipContent({
         data-slot="tooltip-content"
         sideOffset={sideOffset}
         className={cn(
-          "z-[100] w-fit origin-(--radix-tooltip-content-transform-origin) animate-in rounded-md bg-ink px-3 py-1.5 text-xs text-balance text-canvas fade-in-0 zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 shadow-float",
+          "z-[100] w-fit origin-(--radix-tooltip-content-transform-origin) animate-in rounded-md bg-deep text-xs text-balance fade-in-0 zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 max-w-xs text-micro p-2.5 shadow-float leading-relaxed",
           className
         )}
         {...props}
